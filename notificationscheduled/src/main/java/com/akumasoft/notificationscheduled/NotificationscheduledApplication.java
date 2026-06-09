88b454ebea3e7586894767e4473a41eb05e46f48
MoysAkuma <moises141294@hotmail.com>
2026-06-08 21:39:07 -0700

    Initialize notification scheduled service with Maven setup and basic application structure

---
+++ notificationscheduled/src/main/java/com/akumasoft/notificationscheduled/NotificationscheduledApplication.java
@@ -0,0 +1,13 @@
+package com.akumasoft.notificationscheduled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationscheduledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationscheduledApplication.class, args);
+	}
+
+}
---
+++ notificationscheduled/src/test/java/com/akumasoft/notificationscheduled/NotificationscheduledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akumasoft.notificationscheduled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationscheduledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
